4622bbd711dce8cd2b6570d7a223fc8b7a93f576
Battsetseg <battsetseg@LEAP-01s-iMac.local>
2021-05-18 11:10:52 +0800

    home page UI

---
+++ src/pages/home/index.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+import './home.scss';
+const Home = () =>{
+
+    return(
+            <div className="home-container">   
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"></link>            
+                <div className="container">
+                <h1 className="text-center country">MONGOLIA</h1>
+                    <button className="lenses-icon"><i className="fas fa-clone fa-3x" ></i></button>
+                    <button className="footprints-icon"><i className="fas fa-crown fa-3x" ></i></button>
+                    <button className="person-location-icon"><i className="far fa-dot-circle fa-3x" ></i></button>
+                    <button className="world-icon"><i className="fas fa-globe-americas fa-3x" ></i></button>
+                    <button className="message-icon"><i className="fas fa-comment-dots fa-3x" ></i></button>
+                    <button className="add-friend-icon"><i className="fas fa-plus-square fa-3x" ></i></button>
+                    <button className="profile-icon"><i className="fas fa-user fa-3x" ></i></button>
+                </div>
+            </div>    
+    )
+}
+
+
+export default Home;
